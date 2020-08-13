cfdbe7015f9a18fd109013323a3c2b6c69f68bf5
Matt Werth <matthewwerth@gmail.com>
2020-08-13 11:35:50 -0400

    Added Form To Create Post & Create Route

--- app.js
+++ app.js
@@ -47,6 +47,19 @@ app.get("/blogs", (req, res) => {
     })
 })
 
+//NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+  res.render("new")
+})
+
+//CREATE ROUTE
+app.post("/blogs", (req, res) => {
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if(err) res.render("new")
+    else res.redirect("/blogs")
+  })
+})
+
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
